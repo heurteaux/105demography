6965622c56f5d28fbae9e25c241cdd2941a13895
Colin Heurteaux <colin.heurteaux@epitech.eu>
2024-01-18 09:19:21 +0100

    Added proper display of concerned country

--- parse_csv.py
+++ parse_csv.py
@@ -3,20 +3,28 @@ from maths import *
 
 def get_population_array(data, args):
     population_array = [0 for _ in range(len(next(data)) - 2)]
+    country_arr = []
 
     for row in data:
         if row[1] in args[1:]:
+            country_arr.append(row[0])
             for i in range(2, len(row)):
                 population_array[i - 2] += int(row[i])
-    return population_array
+    return population_array, country_arr
 
 def parse_csv(args):
     population_array = None
     try:
         with open("105demography_data.csv", 'r') as file:
             data = csv.reader(file, delimiter=';')
-            population_array = get_population_array(data, args)
+            population_array, country_arr = get_population_array(data, args)
     except:
         print("105demograpy: Demography data file \"105_demography_data.csv\" file cannot be opened or read.")
         exit(84)
+    print("Country: ", end="")
+    for i in range(len(country_arr)):
+        if (i != len(country_arr) - 1):
+            print("{},".format(country_arr[i]), end=" ")
+        else:
+            print("{}".format(country_arr[i]))
     do_math(population_array)
