b3787571bbb53e3240c2f486d62a5a6cd9273eea
Colin Heurteaux <colin.heurteaux@epitech.eu>
2024-01-18 13:32:32 +0100

    Added error handling for many edge cases

--- parse_csv.py
+++ parse_csv.py
@@ -10,18 +10,30 @@ def get_population_array(data, args):
             country_arr.append(row[0])
             for i in range(2, len(row)):
                 population_array[i - 2] += int(row[i])
+
+    if (len(country_arr) != len(args) - 1):
+        print("105demography: Was unable to find data for one of the country codes. Please enter a valid country code.")
+        exit(84)
     return population_array, country_arr
 
 def parse_csv(args):
     population_array = None
     try:
-        with open("105demography_data.csv", 'r') as file:
-            data = csv.reader(file, delimiter=';')
-            population_array, country_arr = get_population_array(data, args)
+        file = open("105demography_data.csv", 'r')
     except:
-        print("105demograpy: Demography data file \"105_demography_data.csv\" file cannot be opened or read.")
+        print("105demography: Demography data file \"105_demography_data.csv\" file cannot be opened or read.")
         exit(84)
+    
+    try:
+        data = csv.reader(file, delimiter=';')
+    except:
+        print("105demography: Cannot read csv data. Please make the csv file valid")
+        exit(84)
+        
+    population_array, country_arr = get_population_array(data, args)
+
     print("Country: ", end="")
+    
     for i in range(len(country_arr)):
         if (i != len(country_arr) - 1):
             print("{},".format(country_arr[i]), end=" ")
